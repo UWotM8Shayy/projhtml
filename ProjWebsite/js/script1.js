0af21f45ee07c07104dab3ec0a8ecdf8616a8746
Shay <135309827+UWotM8Shayy@users.noreply.github.com>
2023-10-24 16:50:19 +0300

    Update script1.js

--- ProjWebsite/js/script1.js
+++ ProjWebsite/js/script1.js
@@ -1,17 +1,17 @@
 
 
-// Wait for the page to load
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the loader container element
+
     const loaderContainer = document.querySelector('.loading-wave-container');
 
-    // Set a timeout to hide the loader container after 1 second
+  
     setTimeout(function() {
         loaderContainer.style.opacity = 0;
         setTimeout(function() {
             loaderContainer.style.display = 'none';
-        }, 500); // Wait for the fade-out transition to complete before hiding the loader
-    }, 500); // 1 second delay before hiding the loader
+        }, 500);
+    }, 500); 
 });
 
 
@@ -25,12 +25,12 @@ function toggleMenu() {
     if (!menuOpen) {
         sideMenu.style.left = '0';
         bars.forEach(bar => {
-            bar.style.backgroundColor = '#fff'; // Change the color to white
+            bar.style.backgroundColor = '#fff'; 
         });
     } else {
         sideMenu.style.left = '-250px';
         bars.forEach(bar => {
-            bar.style.backgroundColor = '#333'; // Change the color back to the original
+            bar.style.backgroundColor = '#333'; 
         });
     }
 
